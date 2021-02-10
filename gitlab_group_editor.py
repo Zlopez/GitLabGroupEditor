103400b4ef3ec90e7ef18f98ffeb68420b55198c
Michal Konečný <mkonecny@redhat.com>
2021-02-10 12:46:32 +0100

    Add mutation
    Signed-off-by: Michal Konečný <mkonecny@redhat.com>

--- gitlab_group_editor.py
+++ gitlab_group_editor.py
@@ -4,15 +4,24 @@
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
-def create_group_query(group: str):
+
+def create_group_query(group: str) -> gql:
+    """
+    Create query for GraphQL API.
+
+    Params:
+      group: Group name which we want to get.
+
+    Returns:
+      Query for GraphQL.
+    """
     return gql(
         """
         query{{
             group(fullPath: "{group}") {{
                 projects {{
                     nodes {{
-                        name
-                        fullPath
+                        id
                         visibility
                         mergeRequestsEnabled
                     }}
@@ -22,13 +31,75 @@ def create_group_query(group: str):
         """.format(group = group)
     )
 
+
+def validate_result(result: dict) -> bool:
+    """
+    Validates the result retrieved from GraphQL.
+
+    Params:
+      result: Response retrieved from GraphQL
+
+    Returns:
+      Result of validation.
+    """
+    try:
+        if result["group"]["projects"]["nodes"]:
+            return True
+    except TypeError:
+        return False
+
+
+def create_projects_mutation(projects: list, visibility: str, merge_requests_enabled: bool) -> gql:
+    """
+    Prepares GraphQL mutation for provided projects.
+
+    Params:
+      projects: List of projects to change.
+      visibility: Visibility of the project, could be "private" or "public".
+      merge_requests_enabled: Enable/disable merge requests on the project.
+
+    Returns:
+      GraphQL mutation.
+    """
+    result = "mutation{"
+    for project in projects:
+        result = result + """
+        project(
+            input: {{
+                id: "{id}",
+                visibility: "{visibility}",
+                mergeRequestsEnabled: "{merge_requests_enabled}"
+            }}
+        ) {{
+            project{{
+                name
+            }}
+            errors
+        }}
+        """.format(
+            id=project["id"],
+            visibility=visibility,
+            merge_requests_enabled=merge_requests_enabled
+        )
+
+
+    result = result + "}"
+    return result
+
+
 if __name__ == "__main__":
     # Let's add API key to header, otherwise only read access to API
-    header = {}
+    token = "foobar"
+    headers = {
+        "Authorization": "Bearer {token}".format(token=token)
+    }
     url = "https://gitlab.com/api/graphql"
     group = "testgroup519"
+    visibility = "private"
+    merge_requests_enabled = False
     # Transport endpoint
-    if header:
+    if headers:
+        print("Using headers: {headers}".format(headers=headers))
         transport = AIOHTTPTransport(url=url, headers=headers)
     else:
         transport = AIOHTTPTransport(url=url)
@@ -41,4 +112,21 @@ if __name__ == "__main__":
 
     # Execute the query
     result = client.execute(query)
-    print(result)
+    # print(result)
+
+    # Get projects from the result
+    if (validate_result(result)):
+        projects = result["group"]["projects"]["nodes"]
+    else:
+        print("The response is missing the projects data: '{response}'".format(response = result))
+        exit(1)
+
+    # print(projects)
+
+    # Prepare mutation
+    mutation = create_projects_mutation(projects, visibility, merge_requests_enabled)
+    print(mutation)
+
+    # Execute the query
+    #result = client.execute(mutation)
+    #print(result)
